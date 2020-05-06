6e8ac1e8c57406d36c6d01e71f6f1f804318cf79
Dylan Stef Torres Walker <48809822+StefWalker@users.noreply.github.com>
2020-05-06 17:17:27 -0600

    Update CRUD_Usuario.sql

--- PrograBD/CRUD's/CRUD_Usuario.sql
+++ PrograBD/CRUD's/CRUD_Usuario.sql
@@ -93,22 +93,3 @@ WHERE  ID_Usuario= @ID_Usuario
  
 END
 GO
-
-
-USE [ProyectoBases]
--- Read de tabla Usuario 
-IF OBJECT_ID('UsuarioSearch') IS NOT NULL
-BEGIN 
-    DROP PROC UsuarioSearch
-END 
-GO
-CREATE PROC UsuarioSearch
-    @Nombre VARCHAR (100),
-    @Password VARCHAR (100)
-AS 
-BEGIN 
-    SELECT ID_Usuario,Nombre,Password,TipoUsuario
-	FROM   Usuario
-    WHERE  (Nombre = @Nombre AND Password = @Password) 
-END
-GO
