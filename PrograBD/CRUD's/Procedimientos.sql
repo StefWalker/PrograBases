0bdd83de60e5e1d4cb69517e8b2a7ff7d80c5e07
Dylan Stef Torres Walker <48809822+StefWalker@users.noreply.github.com>
2020-05-13 16:30:41 -0600

    Procedimientos Update

--- PrograBD/CRUD's/Procedimientos.sql
+++ PrograBD/CRUD's/Procedimientos.sql
@@ -123,3 +123,109 @@ FROM   Propiedad
 WHERE  NumPropiedad = @NumPropiedad
 END
 GO
+
+
+USE [ProyectoBases]
+-- Update de tabla propiedad
+IF OBJECT_ID('PropiedadUpdateB') IS NOT NULL
+BEGIN 
+DROP PROC PropiedadUpdateB
+END 
+GO
+CREATE PROC PropiedadUpdateB
+	 @ID_Propiedad INT,
+	 @NumPropiedad INT,
+	 @Valor MONEY,
+	 @Descripción VARCHAR(250),
+	 @Direccion VARCHAR(250),
+	 @Fecha_Creacion DATE
+AS 
+BEGIN 
+IF(@Valor IS NULL)
+BEGIN
+	SET @Valor = (SELECT Valor FROM Propiedad WHERE NumPropiedad = @NumPropiedad)
+END
+IF(@Descripción IS NULL)
+BEGIN
+	SET @Descripción = (SELECT Descripción FROM Propiedad WHERE NumPropiedad = @NumPropiedad)
+END
+IF(@Direccion IS NULL)
+BEGIN
+	SET @Direccion = (SELECT Direccion FROM Propiedad WHERE NumPropiedad = @NumPropiedad)
+END
+UPDATE Propiedad
+SET  NumPropiedad = @NumPropiedad,
+	 Valor = @Valor,
+	 Descripción	= @Descripción,
+	 Direccion = @Direccion,
+	 Fecha_Creacion = @Fecha_Creacion
+WHERE  (NumPropiedad = @NumPropiedad)
+END
+GO
+
+
+-- Update de tabla Usuario 
+IF OBJECT_ID('UsuarioUpdateB') IS NOT NULL
+BEGIN 
+DROP PROC UsuarioUpdateB
+END 
+GO
+CREATE PROC UsuarioUpdateB
+	 @ID_Usuario INT ,
+	 @Nombre VARCHAR (100),
+	 @NewName VARCHAR (100),
+     @Password VARCHAR (100),
+	 @NewPassword VARCHAR (100),
+     @TipoUsuario VARCHAR (100)
+AS 
+BEGIN 
+IF(@NewName IS NULL)
+BEGIN
+	SET @NewName = (SELECT Nombre FROM Usuario WHERE (Nombre = @Nombre AND Password = @Password))
+END
+IF(@NewPassword IS NULL)
+BEGIN
+	SET @NewPassword = (SELECT Password FROM Usuario WHERE (Nombre = @Nombre AND Password = @Password))
+END
+IF(@TipoUsuario IS NULL)
+BEGIN
+	SET @TipoUsuario = (SELECT TipoUsuario FROM Usuario WHERE (Nombre = @Nombre AND Password = @Password))
+END
+UPDATE Usuario
+SET  Nombre = @NewName,
+	 Password = @NewPassword,
+	 TipoUsuario = @TipoUsuario
+
+WHERE  (Nombre = @Nombre AND Password = @Password)
+END
+GO
+
+
+-- Update de tabla propietario 
+IF OBJECT_ID('PropietarioUpdateB') IS NOT NULL
+BEGIN 
+DROP PROC PropietarioUpdateB
+END 
+GO
+CREATE PROC PropietarioUpdateB
+	 @ID_Propietario INT,
+	 @Identificacion INT,
+	 @NewIdentificacion INT,
+	 @Nombre VARCHAR (100),
+	 @NewNombre VARCHAR (100)
+AS 
+BEGIN
+IF(@NewIdentificacion IS NULL)
+BEGIN
+	SET @NewIdentificacion = (SELECT Identificacion FROM Propietario WHERE Identificacion = @Identificacion)
+END
+IF(@NewNombre IS NULL)
+BEGIN
+	SET @NewNombre = (SELECT Nombre FROM Propietario WHERE Identificacion = @Identificacion)
+END
+UPDATE Propietario
+SET  Identificacion= @Identificacion,
+	 Nombre = @Nombre
+WHERE  (Identificacion = @Identificacion)
+END
+GO
