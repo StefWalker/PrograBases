48c6e06478205e123890407e5d40c4096f282c23
FerLanza <43628127+FerLanza@users.noreply.github.com>
2020-06-03 16:48:04 -0600

    Fechas cambios

--- WebAplication/WebApplication1/frmAgregarPropiedadUsuario.aspx.designer.cs
+++ WebAplication/WebApplication1/frmAgregarPropiedadUsuario.aspx.designer.cs
@@ -60,15 +60,6 @@ namespace WebApplication1
         protected global::System.Web.UI.WebControls.TextBox txtDireccion;
 
         /// <summary>
-        /// Control txtFecha.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtFecha;
-
-        /// <summary>
         /// Control lblerror.
         /// </summary>
         /// <remarks>
