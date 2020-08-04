ef0123d3c201b2ef480f3ba9c1339e0229116575
FerLanza <43628127+FerLanza@users.noreply.github.com>
2020-08-03 22:00:37 -0600

    Agregue el metodo para cancelar la transaccion

--- PrograBD/LoNewDB/RollbackPagos.sql
+++ PrograBD/LoNewDB/RollbackPagos.sql
@@ -8,10 +8,23 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE PROC [dbo].[RollBackPagos] @inValue int
-AS  
-	begin 
-		IF(@inValue = 1)Begin ROLLBACK END
-		Else
-			BEGIN COMMIT END
-	end
+CREATE PROC [dbo].[RollBackPagos]
+AS 
+BEGIN
+	Declare @i INT 
+	Declare @contador INT 
+	Declare @IDRecibo INT 
+	Set @i=1
+	Set @contador = (Select max(id) from tmp)
+	while (@i <= @contador)
+		BEGIN
+			Set @IDRecibo = (Select ID_Recibo FROM Recibos INNER JOIN tmp on tmp.idRecibo = Recibos.ID_Recibo where tmp.id = @i)
+			UPDATE Recibos 
+		    SET	Estado = 3  
+			WHERE  @IDRecibo = Recibos.ID_Recibo AND Recibos.ID_Concepto= 11
+			Set @i = @i+1
+					
+		END 
+		DELETE FROM tmp;
+			
+END 
