c2156a4f590d71963e195f028633288000a90e6a
Dylan Stef Torres Walker <48809822+StefWalker@users.noreply.github.com>
2020-08-04 23:22:02 -0600

    Adicion de SP Arreglos

--- PrograBD/LoNewDB/crearTipos.sql
+++ PrograBD/LoNewDB/crearTipos.sql
@@ -3,6 +3,7 @@ CREATE TYPE PagosTipo AS TABLE (id INT IDENTITY(1,1),numFinca INT,idTipoRecibo I
 CREATE TYPE ConsumoTipo AS TABLE (id INT IDENTITY(1,1),idTipo INT,numFinca INT,LecturaM3 INT, Fecha DATE,descripcion varchar(30)) 
 CREATE TYPE ValorPropiedadTipo AS TABLE (id INT IDENTITY(1,1),numFinca INT,nuevoValor MONEY) 
 CREATE TYPE RecibosTipo AS TABLE (id INT IDENTITY(1,1), idRecibo int) 
+CREATE TYPE ArreglosTipo AS TABLE (ID INT IDENTITY(1,1), numFinca int, plazo int, fecha DATE)
 
 /*
 
@@ -18,3 +19,4 @@ DROP TYPE PagosTipo
 DROP TYPE ConsumoTipo
 DROP TYPE ValorPropiedadTipo
 DROP TYPE RecibosTipo
+DROP TYPE ArreglosTipo
